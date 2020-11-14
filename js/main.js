709459c6a94425e3ebe24d16cb89b610da3a71f9
Danuka Ashan <danukaashan886@gmail.com>
2020-11-14 17:57:16 +0530

    basic-functions

--- js/main.js
+++ js/main.js
@@ -26,9 +26,15 @@ document.querySelector('.btn-roll').addEventListener('click',function (){
         round += dice;
         document.querySelector('#current-' + activePlayer).textContent = round;
     }else{
-        round=0;
         activePlayer===0?activePlayer =1:activePlayer=0;
+        round=0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
 
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        dice1.style.display = 'none';
     }
 
 });
